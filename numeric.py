f623110e4bc3bbad48c9da5d8a7bc3c2e093f22a
jowin202 <35803880+jowin202@users.noreply.github.com>
2020-06-11 12:36:02 +0200

    Add files via upload

--- numeric.py
+++ numeric.py
@@ -1,6 +1,7 @@
 import time
 import csv
 import matplotlib.pyplot as plt
+from statistics import mean
 
 
 def PolygonArea(x, y):
@@ -11,6 +12,7 @@ def PolygonArea(x, y):
         A += x[i] * y[j]
         A -= x[j] * y[i]
     A = abs(A)/2
+    A *= 0.0001 # change unit to 1 Joule
     return A
 
 
@@ -57,10 +59,21 @@ def CalcAreaTimeSeries(p, V):
     Areas = []
     for i in range(len(maxima)-1):
         Areas.append(PolygonArea(p[maxima[i]:maxima[i+1]], V[maxima[i]:maxima[i+1]] ))
-    print(Areas)
-                
+    return Areas
 
 
+def CalcCycleTimeSeries(p, V):
+    maxima = findMax(p)
+    
+    result = []
+    for i in range(len(maxima)-1):
+        result.append(maxima[i+1]-maxima[i])
+    return result
+
+def analyzeAreas(Areas):
+    mw = mean(Areas)
+    print("Mittelwert: " + str(mw))
+    print("Spannweite: " + str(max(Areas) - min(Areas)))
 
 
 
@@ -96,5 +109,77 @@ plt.close()
 
 
 # Flaechen
-CalcAreaTimeSeries(druck_un, vol_un)
-CalcAreaTimeSeries(druck_be, vol_be)
+print("Unbelelastet: ")
+area_un = CalcAreaTimeSeries(druck_un, vol_un)
+analyzeAreas(area_un)
+print("Belelastet: ")
+area_be = CalcAreaTimeSeries(druck_be, vol_be)
+analyzeAreas(area_be)
+
+
+# doing a fancy latex output for copy & paste to script
+print("Area per round: ")
+for i in range(max(len(area_un), len(area_be))):
+    print(str(i+1) + " & " + str(round(area_un[i],4) if i < len(area_un) else "") + " & " + str(round(area_be[i],4)) + " \\\\")
+
+print("Zyklusdauern: ")
+cycl_un = CalcCycleTimeSeries(druck_un, vol_un)
+cycl_be = CalcCycleTimeSeries(druck_be, vol_be)
+
+for i in range(max(len(cycl_un), len(cycl_be))):
+    print(str(i+1) + " & " + str(cycl_un[i] if i < len(cycl_un) else "") + " & " + str(cycl_be[i]) + " \\\\")
+
+print("\\hline")
+print("Mittelwert: & " + str(mean(cycl_un)) + " & " + str(mean(cycl_be)) + " \\\\")
+print("Spannweite: & " + str(max(cycl_un)-min(cycl_un)) + " & " + str(max(cycl_be)-min(cycl_be)) + " \\\\")
+
+
+print("")
+print("")
+
+delta_t = max(max(cycl_un)-min(cycl_un), max(cycl_be)-min(cycl_be)) #spannweite
+delta_t /= 1000 # in sekunden
+
+spannweite_area_un = max(area_un) - min(area_un)
+spannweite_area_be = max(area_be) - min(area_be)
+
+mean_area_un = mean(area_un)
+mean_area_be = mean(area_be)
+
+mean_cycl_un = mean(cycl_un)/1000
+mean_cycl_be = mean(cycl_be)/1000
+
+print("Leistungsberechnung: ")
+print("Leistung unbelastet: " + str(mean_area_un/mean_cycl_un))
+print("Leistung belastet: " + str(mean_area_be/mean_cycl_be))
+print("Leistung_unsicherheit unbelastet: " + str(spannweite_area_un/mean_cycl_un + mean_area_un/mean_cycl_un**2 * delta_t))
+print("Leistung belastet: " + str(spannweite_area_be/mean_cycl_be + mean_area_be/mean_cycl_be**2 * delta_t))
+
+
+
+
+
+
+print("")
+print("")
+
+print("Mit Wasser abtransportierte Leistung:")
+rho_w = 998
+c = 4186
+T_un = 5
+T_be = 6.1
+dVpdt = 200.0/49 * 10**-6
+Delta_Temp = 0.25
+Delta_time = 0.1
+Delta_Vol = 1.5*10**-6
+
+V_W = 200*10**-6
+t_W = 49
+
+print("P unbelastet: " + str(rho_w * c * T_un * dVpdt))
+print("P belastet: " + str(rho_w * c * T_be * dVpdt))
+
+
+print("Unsicherheit unbelastet: " + str( rho_w * c * ( Delta_Temp *  V_W/t_W + T_un * Delta_Vol/t_W + T_un * V_W / t_W**2 * Delta_time  )))
+print("Unsicherheit belastet: " + str( rho_w * c * ( Delta_Temp *  V_W/t_W  + T_be * Delta_Vol/t_W + T_be * V_W / t_W**2 * Delta_time  )))
+
